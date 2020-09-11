bd4f06e1453c254cb9fc043bbd84077c64f91b93
jedgodsey <jed.godsey@gmail.com>
2020-09-10 20:34:15 -0700

    Commit 5 - Getting to Know You answered

--- scripts/app.js
+++ scripts/app.js
@@ -11,56 +11,42 @@
 
 // ## Excited Kitten
 
-for (let i = 1; i <= 20; i++) {
-    console.log("Love me, pet me! HSSSSSS!");
-    let catResponse = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
-    if (i % 2 === 0) {
-        console.log(catResponse[Math.floor(Math.random() * 3)]);
-    }
-}
+// for (let i = 1; i <= 20; i++) {
+//     console.log("Love me, pet me! HSSSSSS!");
+//     let catResponse = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+//     if (i % 2 === 0) {
+//         console.log(catResponse[Math.floor(Math.random() * 3)]);
+//     }
+// }
 
 // ## Fizz Buzz 
-
-// >NOTE: THIS IS A CLASSIC RITE-OF-PASSAGE FOR JAVASCRIPT PROGRAMMERS, ESPECIALLY ONES AT GA
-
-// Write a javascript application that logs all numbers from 1 - 100 **AND**: 
-
-//   1. If a number is divisible by 3 log "Fizz" instead of the number.
-  
-//   1. If a number is divisible by 5 log "Buzz" instead of the number.
-  
-//   1. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number, "Fizz", or "Buzz".
 
 for (let i = 1; i <= 100; i++) {
     i % 3 === 0 ? (i % 5 === 0 ? console.log('FizzBuzz') : console.log('Fizz')) : i % 5 === 0 ? console.log('Buzz') : console.log(i);
 }
-// <hr>
-// &#x1F534; The commit message should read: <br>
-// "Commit 4 - Fizz Buzz answered"
-// <hr>
 
 // ## Getting to Know You
-// Use the following arrays to answer the questions below (name, age, hometown):
-// ```javascript
-// const kenny = ["Kenny", 1000, "Austin"];
-// const jimHaff = ["Jim H", 16, "All cities"];
-// const reuben = ["Reuben", 22, "Durham"];
-// const jimClark = ["Jim C", 186, "LA"];
-// const ryan = ["Ryan", 65, "Denver"];
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
 // ```
 // 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. Remove "Kenny" from the `kenny` array and replace it with "Gameboy".
-
+kenny[0] = 'Gambeboy';
 // 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older.  Don't just hard code `187`--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
-
+jimClark[1] += 1;
 // 3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
-
+ryan[2] = 'Gotham City';
 // 4. Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
-
+reuben.pop;
+reuben.push('Chicago');
 // 5. Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but [see if you can do it in one line of code](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
-
+jimHaff.splice(2,1,'San Jose', 'Oakland', 'Santa Rosa');
 // 6. Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities.  Whatever the _first_ of the 3 cities for Jim Haff is now, remove it from the array using [`.splice()`](https://www.google.com/search?q=mdn+splice&oq=mdn+splice&aqs=chrome.0.0j69i60j0l4.1078j0j7&sourceid=chrome&ie=UTF-8)
+jimHaff.splice(2,1);
 
-// <hr>
 // &#x1F534; The commit message should read: <br>
 // "Commit 5 - Getting to Know You answered"
 // <hr>
